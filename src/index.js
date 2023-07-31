ee6fc729c4797f71ace7edb77bcd22e1a5905363
Alexey19800506 <osiyuk19800506@gmail.com>
2023-07-31 15:34:27 +0300

    task3

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import "./styles.css"
+ 
+const rootElement = document.querySelector('#root');
+
+const element = ( 
+<div className = "greeting" >
+   <h1 className = "greeting-title" > Hello, world! </h1>
+   <p className = "greeting-text" > I 'm learning React</p>
+</div>  
+);
+
+ReactDOM.render(element, rootElement);
