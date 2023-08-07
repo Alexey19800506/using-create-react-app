e8ec39586e0eb3cfcd635e0194fa406326a05dd0
Alexey19800506 <osiyuk19800506@gmail.com>
2023-08-07 11:56:05 +0300

    t3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import "./styles.css"
+import ReactDOM from 'react-dom';
+import "./styles.css";
  
 const rootElement = document.querySelector('#root');
 
 const element = ( 
 <div className = "greeting" >
-   <h1 className = "greeting-title" > Hello, world! </h1>
-   <p className = "greeting-text" > I'm learning React</p>
+   <h1 className = "greeting__title" > Hello, world! </h1>
+   <p className = "greeting__text" > I'm learning React</p>
 </div>  
 );
 
